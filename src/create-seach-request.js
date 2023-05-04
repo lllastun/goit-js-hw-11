ad2645e3c96c7c789a80a39b4f6c23b6cd68fb1e
lllastun <lllastun@gmail.com>
2023-05-04 18:33:02 +0300

    01

--- src/create-seach-request.js
+++ src/create-seach-request.js
@@ -1,4 +1,4 @@
-const API = 'https://pixabay.com/api';
+const API = 'https://pixabay.com/api/';
 const API_KEY = '35997534-743ef0023491800c2d14ecb92';
 const REQUEST_PARAMETRS = 'image_type=photo&orientation=horizontal&safesearch=true';
 const perPage = 40;
